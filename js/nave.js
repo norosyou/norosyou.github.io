8aa09348202b4f79d39cb46073046c130b823ada
norosyou <syouitinoro@gmail.com>
2023-02-08 17:16:25 +0900

    main

--- js/nave.js
+++ js/nave.js
@@ -1,20 +1,19 @@
 //ボタンがクリックされたら
-$(".openbtn1").click(
-	function ()
-	{
-		//ボタン自身に activeクラスを付与し
-		$(this).toggleClass('active');
-		//ナビゲーションにpanelactiveクラスを付与
-		$("#g-nav").toggleClass('panelactive');
-	}
-);
-
-//ナビゲーションのリンクがクリックされたら
-$("#g-nav a").click(
-	function () 
-	{
-		//ボタンの activeクラスを除去し
-		$(".openbtn1").removeClass('active');
-		//ナビゲーションのpanelactiveクラスも除去
-		$("#g-nav").removeClass('panelactive');
-});
+$(".openbtn1").click(function () {
+	//ボタン自身に activeクラスを付与し
+	$(this).toggleClass('active');
+	//ナビゲーションにpanelactiveクラスを付与
+	$("#g-nav").toggleClass('panelactive');
+	//丸背景にcircleactiveクラスを付与
+	$(".circle-bg").toggleClass('circleactive');
+  });
+  
+  //ナビゲーションのリンクがクリックされたら
+  $("#g-nav a").click(function () {
+	//ボタンの activeクラスを除去し
+	$(".openbtn1").removeClass('active');
+	//ナビゲーションのpanelactiveクラスを除去
+	$("#g-nav").removeClass('panelactive');
+	//丸背景のcircleactiveクラスを除去
+	$(".circle-bg").removeClass('circleactive');
+  });
