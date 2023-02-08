d8812c3b74b781c93cf78fb16797ff5ecde73c54
norosyou <syouitinoro@gmail.com>
2023-02-08 18:36:39 +0900

    main

--- js/script.js
+++ js/script.js
@@ -17,3 +17,11 @@ $(".openbtn1").click(function () {
 	//丸背景のcircleactiveクラスを除去
 	$(".circle-bg").removeClass('circleactive');
   });
+
+  // #page-topをクリックした際の設定
+$('#page-top').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;//リンク自体の無効化
+});
